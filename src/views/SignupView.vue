79e5e52d5abf91307e8c8978a271579b93bb7c87
mac <mac@MacBook-Pro-de-mac-7.local>
2025-04-14 11:35:51 +0100

    include bootstrap

---
+++ src/views/SignupView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Login</h1>
+    <label for="email">Email</label>
+    <input type="text" placeholder="Email" />
+    <br>
+
+    <label for="password">Password</label>
+    <input type="password" placeholder="Password" />
+
+  </div>
+</template>
+
+<script setup>
+
+
+
+</script>
