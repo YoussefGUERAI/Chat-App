f32e2afb7ddec4209e46c1379350d08ab6fa954e
Anir Achibane <anirf42@gmail.com>
2025-04-14 22:21:52 +0100

    add composables to fetch from database

---
+++ src/composables/getConversations.js
@@ -0,0 +1,26 @@
+import {db, auth} from "@/firebase/config";
+import {onMounted} from "vue";
+import { ref } from "vue";
+
+const conversations = ref([]);
+
+async function getConversations(){
+    onMounted( async () => {
+        await db.collection('chat').where('users', 'array-contains', auth.currentUser.uid)
+        .onSnapshot((snapshot) => {
+            conversations.value.push(snapshot.docs.map((doc) => doc.data()));
+        });
+
+        await db.collection('group').where('users','array-contains',auth.currentUser.uid)
+        .onSnapshot((snapshot) => {
+            conversations.value.push(snapshot.docs.map((doc) => doc.data()))
+        });
+
+    conversations.sort((a, b) => a.lastUpdate - b.lastUpdate);  
+    });
+
+    return { conversations };
+
+}
+
+export { getConversations, conversations };
