a664787c1ac5f0ad61987be22ac482eb13377464
mac <mac@MacBook-Pro-de-mac-7.local>
2025-04-14 12:30:39 +0100

    add login and signup pages

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,23 @@
-import firebase from 'firebase/app' // the core part of firebase package
-// Then we import any features from firebase that we want to use // in our case it is cloud firestore
-import 'firebase/firestore'
-const firebaseConfig = {
-    apiKey: "AIzaSyAAKvvfaguX8l4LD0xjqIggxhQqLmpnvZ8",
-    authDomain: "chat-app-16fdd.firebaseapp.com",
-    projectId: "chat-app-16fdd",
-    storageBucket: "chat-app-16fdd.firebasestorage.app",
-    messagingSenderId: "161988951827",
-    appId: "1:161988951827:web:ab51ddc8203bcdef8c64e6"
-  };
-// Step 1 : init firebase
-// we use the firebaseConfig to init the connection to our firebase backend firebase.initializeApp(firebaseConfig)
-// Step 2 : Init the firestore service
-// If we want to do any communication with the firestore db (eg. add documents)
-// we can use projectFirestore variable
-const projectFirestore = firebase.firestore()
-//Step 3 : Export projectFirestore
-// This will enable the use the firebase in different components export {projectFirestore}
+// firebase/config.js
+import firebase from 'firebase/app'           // core Firebase
+import 'firebase/firestore'                  // Firestore
+import 'firebase/auth'                       // line for Auth
 
-export{projectFirestore}
+const firebaseConfig = {
+  apiKey: "AIzaSyAAKvvfaguX8l4LD0xjqIggxhQqLmpnvZ8",
+  authDomain: "chat-app-16fdd.firebaseapp.com",
+  projectId: "chat-app-16fdd",
+  storageBucket: "chat-app-16fdd.firebasestorage.app",
+  messagingSenderId: "161988951827",
+  appId: "1:161988951827:web:ab51ddc8203bcdef8c64e6"
+};
+
+// Step 1: Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Step 2: Initialize services
+const db = firebase.firestore();
+const auth = firebase.auth();          // ✅ Auth service
+
+// Step 3: Export services
+export { db, auth };
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,53 @@
 <template>
-  <div class="home">
-    
-    
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card mt-5">
+          <div class="card-body">
+            <h1 class="card-title text-center">Login</h1>
+            <form>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" placeholder="Email" v-model="email" />
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" placeholder="Password" v-model="password" />
+              </div>
+              <button class="btn btn-primary" @click="login">Login</button>
+            </form>
+            <div class="mt-3 text-center">
+              New to this website? <router-link to="/signup">Signup</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { auth } from '../firebase/config'
 
 
-export default {
-  name: 'HomeView',
-  
+const email = ref('')
+const password = ref('')
+const router = useRouter()
+
+const login = async () => {
+  try {
+    const res = await auth.signInWithEmailAndPassword(email.value, password.value)
+    console.log(res)
+    router.push('/')
+  } catch (err) {
+    console.log(err.message)
+  }
 }
+
+
+
+
 </script>
